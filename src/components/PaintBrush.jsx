0db335deb2e97955f4e50196294cf2e59eec4925
AceDZN <design@acedzn.com>
2016-03-28 03:09:46 +0300

    + Added draw & brush color\size functionality

--- src/components/PaintBrush.jsx
+++ src/components/PaintBrush.jsx
@@ -5,33 +5,79 @@ class PaintBrush extends Component {
   constructor(props){
     super(props);
     this.state = {
-      renderedImg:""
+      drawup: false,
+      brushColor: 'black',
+      brushSize: '5'
     }
   }
   componentWillMount(){
+    this.mouse = {x:0,y:0};
+    this.lastMouse={x:0,y:0};
 
   }
   componentDidMount() {
-   this.w = this.canvas.clientWidth;
-   this.h = this.canvas.clientHeight;
-   this.ctx = this.canvas.getContext('2d');
-   this.ctx.canvas.width  = this.w;
-   this.ctx.canvas.height = this.h;
-
-
+    this.w = this.canvas.clientWidth;
+    this.h = this.canvas.clientHeight;
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.canvas.width  = this.w;
+    this.ctx.canvas.height = this.h;
   }
+  getMousePosition(evt) {
+        var rect = this.canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+      }
 
-  draw(){
-
+  handleMouseMove(e){
+    this.lastMouse.x=this.mouse.x;
+    this.lastMouse.y=this.mouse.y;
+    this.mouse = this.getMousePosition(e);
+    this.draw('move');
   }
-  handleSave(){
-
+  handleMouseDown(e){
+    this.draw('down');
   }
-  handleClear(){
-
+  handleMouseUp(e){
+    this.draw('up');
   }
-  handleColorPicker(){
-
+  draw(a){
+    if(a == 'down'){
+      this.setState({
+        drawup:true
+      });
+    }
+    if(a == 'up'){
+      this.setState({
+        drawup:false
+      });
+    }
+    if(this.state.drawup){
+      this.ctx.beginPath();
+        this.ctx.moveTo(this.lastMouse.x,this.lastMouse.y);
+        this.ctx.lineTo(this.mouse.x,this.mouse.y);
+        this.ctx.strokeStyle = this.state.brushColor;
+        this.ctx.lineWidth = this.state.brushSize;
+        this.ctx.stroke();
+        this.ctx.closePath();
+    }
+  }
+  handleSave(e){
+    console.log("save");
+  }
+  handleClear(e){
+    console.log("clear");
+  }
+  handleColorPicker(e){
+    this.setState({
+      brushColor: e.target.value
+    });
+  }
+  handleBrushSize(e){
+    this.setState({
+      brushSize: e.target.value
+    });
   }
   render() {
    return (
@@ -43,7 +89,7 @@ class PaintBrush extends Component {
               <span className="input-group-addon btn">
                 <label htmlFor="color">Select Color</label>
               </span>
-              <input type="color" name="color" className="form-control" id="color" onChange={this.handleColorPicker} />
+              <input type="color" name="color" className="form-control" id="color" onChange={this.handleColorPicker.bind(this)} />
             </div>
           </div>
           <div className="form-group">
@@ -51,17 +97,22 @@ class PaintBrush extends Component {
               <span className="input-group-addon">
                 Brush Size:
               </span>
-              <input type="range" min="1" max="20" name="brush" className="form-control" id="color" onChange={this.handleBrushSize} />
+              <input type="range" min="1" max="20" name="brush" className="form-control" id="color" onChange={this.handleBrushSize.bind(this)} />
             </div>
           </div>
         </div>
         <div className="col-sm-3 text-right">
-          <button type="button" className="btn btn-primary" onClick={this.handleSave}>Save</button>
-          <button type="button" className="btn btn-danger" onClick={this.handleClear}>Clear</button>
+          <button type="button" className="btn btn-primary" onClick={this.handleSave.bind(this)}>Save</button>
+          <button type="button" className="btn btn-danger" onClick={this.handleClear.bind(this)}>Clear</button>
         </div>
       </div>
       <div>
-        <canvas ref={(c) => this.canvas = c} />
+        <canvas ref={(c) => this.canvas = c}
+          onMouseMove={this.handleMouseMove.bind(this)}
+          onMouseDown={this.handleMouseDown.bind(this)}
+          onMouseUp={this.handleMouseUp.bind(this)}
+          onMouseOut={this.handleMouseUp.bind(this)}
+          />
       </div>
       <img ref={(img) => this.preview = img} className="" />
     </div>
